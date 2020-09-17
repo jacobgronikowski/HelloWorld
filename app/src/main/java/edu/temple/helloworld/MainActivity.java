bd55f61a23412d736cf5cdf7c4de466bc1202151
Jake <tuh13091@temple.edu>
2020-09-16 22:17:32 -0400

    Finished product, added constraints to button and used the onclick listener to set the text in the textview to a new message

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,10 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button;
+        button = findViewById(R.id.button);
+
+        myOnClickListener onClickListener = new myOnClickListener();
+        button.setOnClickListener(onClickListener);
+
+
+
     }
+    public class myOnClickListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            TextView tv;
+            tv = findViewById(R.id.textView);
+            tv.setText("You have clicked the button");
+        }
+    }
+
 }
